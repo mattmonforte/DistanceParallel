d978e7bca0a4e700c8ba59166ad426899b583d44
Matt Monforte <contact@mattmonforte.com>
2020-10-14 22:08:02 -0700

    working. commit before working on making UnLocker Class.

--- DistanceParallel_cmd.py
+++ DistanceParallel_cmd.py
@@ -12,20 +12,20 @@ import scriptcontext as sc
 
 # clean up how unlock sticky works
 # work on returns False, None, True
-# remember unlock settings
 # Next make able to measure meshes
+# maybe dont unlock hidden layers
 # tell user when block selected so command wont work
-# Next make able to measure objects in blocks
 # if cant measure blocks error check when block selected
 # let user decide if can select locked objects
 # add select again by reseting
 # display command version for user somehow
 # add allow regular distance_cmd measurements
 # add allow selection of point and plane
-# add allow selection of point and line closest point
+# add allow select point and point
 # hightlight bad selection with red hightlght
 # Done-add message out click to copy to clipboard
 # see if can improve hightlighting of block selections
+# only try to relock layers/objcects if unlocked in the first place
 
 __commandname__ = "DistanceParallel"
 def RunCommand( is_interactive ):
@@ -36,113 +36,138 @@ def RunCommand( is_interactive ):
     ZERO_TOLERANCE = 1.0e-5
     myDebug = True
     unlockLayersDefault = False
-    global UnlockLayersUsed
-    UnlockLayersUsed = False
+    global isUnlocked
+    isUnlocked = False
     global layersToRelock
     layersToRelock = []
     global objectsToRelock
     objectsToRelock = []
     global plane
     plane = []
+    selectedObjects = []
+    selectedCounter = 0
 
     if sc.sticky.has_key("distance_parallel_unlock"):
         unlockLayersDefault = sc.sticky["distance_parallel_unlock"]
+    # if sc.sticky.has_key("distance_parallel_modalmsg"):
+        # unlockLayersDefault = sc.sticky["distance_parallel_modalmsg"]
 
-    s1 = rc.Input.Custom.GetObject()
-    s1.SetCommandPrompt("Select Two Parallel Planar Surfaces")
-    s1.AlreadySelectedObjectSelect = False
-    s1.DeselectAllBeforePostSelect = False
-    # s1.EnableClearObjectsOnEntry(False)
-    # s1.EnableUnselectObjectsOnExit(False)
-    s1.OneByOnePostSelect = True
-    s1.SubObjectSelect = True
-    s1.EnablePreSelect(False, True)
-    # s1.EnableClearObjectsOnEntry(False)
+    go = rc.Input.Custom.GetObject()
+    go.SetCommandPrompt("Select Two Parallel Planar Surfaces")
+    go.AlreadySelectedObjectSelect = False
+    go.DeselectAllBeforePostSelect = False
+    go.EnableClearObjectsOnEntry(False)
+    # go.EnableUnselectObjectsOnExit(False)
+    go.OneByOnePostSelect = True
+    go.SubObjectSelect = True
+    go.EnablePreSelect(False, True)
+    # go.EnableClearObjectsOnEntry(False)
 
     ## Filter object type
     geometryType = rc.DocObjects.ObjectType.Surface
-    s1.GeometryFilter = geometryType
+    go.GeometryFilter = geometryType
 
     unlockLayers = rc.Input.Custom.OptionToggle(unlockLayersDefault, "Off", "On")
-    s1.AddOptionToggle("UnlockLayersAndObjects", unlockLayers)
+    go.AddOptionToggle("UnlockLayersAndObjects", unlockLayers)
 
     if unlockLayers.CurrentValue == 1:
-        UnlockLayersUsed = True
         layersToRelock = unlock_layers()
         objectsToRelock = unlock_objects()
+        isUnlocked = True
 
     while True:
-    	getResult = s1.GetMultiple(1,2)
-    	if getResult == rc.Input.GetResult.Cancel:
+        getResult = go.GetMultiple(selectedCounter+1,selectedCounter+1)
+        # go.EnableClearObjectsOnEntry(False)
+        # go.DeselectAllBeforePostSelect = False
+        if getResult == rc.Input.GetResult.Cancel:
             clean_up_cancel()
             return rc.Commands.Result.Cancel
-    	elif getResult == rc.Input.GetResult.Option:
+        # if go.CommandResult() != rc.Commands.Result.Success:
+        #     # why is this triggering?
+        #     clean_up_success()
+        #     return go.CommandResult()
+        if getResult == rc.Input.GetResult.Object:
+            selectedObjects = go.Objects()
+            selectedCounter = len(selectedObjects)
+            # get objects from selections
+            objref = go.Object(selectedCounter-1)
+            # get selected surface object
+            obj = objref.Object()
+            if not obj:
+                # Why is this needed?
+                if myDebug :
+                    msgOut("Object not selected.")
+                clean_up_fail()
+                return rc.Commands.Result.Failure
+            try:
+                surface = objref.Surface()
+            except:
+                msgOut("Surfaces within blocks are not always selectable. Polysurfaces can be selected, Extrusion cannot. Explode block for more selectability.")
+                # try to continue and reselect
+                clean_up_fail()
+                return rc.Commands.Result.Failure
+            if not surface:
+                # why is this needed?
+                if myDebug :
+                    msgOut("No surface selected.")
+                clean_up_fail()
+                return rc.Commands.Result.Failure
+
+            test, getPlane = surface.TryGetPlane(ZERO_TOLERANCE)
+            if not test:
+                msgOut("Surfaces is not planar.")
+                # maybe continue or allow select of first surface again
+                clean_up_fail()
+                return rc.Commands.Result.Failure
+            plane.append(getPlane)
+            if selectedCounter == 2:
+                clean_up_success()
+                break
+            # loop until second surface selected or cancel
+            continue
+        # Locking and Unlocking Objects and Layers
+        elif getResult == rc.Input.GetResult.Option:
             if unlockLayers.CurrentValue == 1:
-                UnlockLayersUsed = True
+                isUnlocked = True
                 sc.sticky["distance_parallel_unlock"] = True
                 layersToRelock = unlock_layers()
                 objectsToRelock = unlock_objects()
             else:
+                isUnlocked = False
                 sc.sticky["distance_parallel_unlock"] = False
                 lock_objects(objectsToRelock)
                 lock_layers(layersToRelock)
-    	    # s1.EnablePreSelect(False, True)
+    	    # go.EnablePreSelect(False, True)
             continue
-        elif getResult == rc.Input.GetResult.Object:
-            # ids = [s1.Object(i).ObjectId for i in range(s1.ObjectCount)]
-            for i in range(2):
-                # get objects from selections
-                objref = s1.Object(i)
-                # get selected surface object
-                obj = objref.Object()
-                if not obj:
-                    if myDebug :
-                        msgOut("Object not selected")
-                    clean_up_fail()
-                    return rc.Commands.Result.Failure
-                try:
-                    surface = objref.Surface()
-                except:
-                    msgOut("Surfaces within blocks are not always selectable. Polysurfaces can be selected, Extrusion cannot. Explode block for more selectability.")
-                    # try to continue and reselect
-                    clean_up_fail()
-                    return rc.Commands.Result.Failure
-                if not surface:
-                    if myDebug :
-                        msgOut("No surface selected.")
-                    clean_up_fail()
-                    return rc.Commands.Result.Failure
-                # Unselect surface
-                test, getPlane = surface.TryGetPlane(ZERO_TOLERANCE)
-                if not test:
-                    msgOut("One of the selected surfaces is not planar.")
-                    # maybe continue or allow select of first surface again
-                    clean_up_fail()
-                    return rc.Commands.Result.Failure
-                plane.append(getPlane)
-                # obj.Select(False)
+        elif getResult == Rhino.Input.GetResult.Nothing:
+            # Why is this needed?
+            msgOut("Got nothing.")
+            clean_up_fail()
+            return rc.Commands.Result.Failure
+			# break
+        # end While
 
-            # Are planes parallel
-            # if ArePlanesParallel(plane[0], plane[1], ZERO_TOLERANCE) == True:
-            if ArePlanesParallel(plane[0], plane[1], ZERO_TOLERANCE) == False:
-                msgOut("Surfaces are not parallel to each other.")
-                clean_up_fail()
-                return rc.Commands.Result.Failure
 
-            # measure distance between planes
-            # acually distance between plane[0] and point on plan[1]
-            ParallelDistance = abs(rs.DistanceToPlane(plane[0], plane[1][0]))
-            # output results
-            unitsName = doc.GetUnitSystemName(True, True, True, False)
-            ## fromating output decimal places not working
-            # decimalPlaces = doc.DistanceDisplayPrecision
-            # textOut = "Parallel Distance = {:." + str(decimalPlaces) +"f} " + unitsName
-            rs.ClipboardText(ParallelDistance)
-            textOut = "Parallel Distance = {:.4f} " + unitsName
-            msgOut(textOut.format(ParallelDistance))
-
-        clean_up_success()
-        return rc.Commands.Result.Success
+    # Are planes parallel
+    # if ArePlanesParallel(plane[0], plane[1], ZERO_TOLERANCE) == True:
+    if ArePlanesParallel(plane[0], plane[1], ZERO_TOLERANCE) == False:
+        msgOut("Surfaces are not parallel to each other.")
+        clean_up_fail()
+        return rc.Commands.Result.Failure
+    # measure distance between planes
+    # acually distance between plane[0] and point on plan[1]
+    ParallelDistance = abs(rs.DistanceToPlane(plane[0], plane[1][0]))
+    # output results
+    unitsName = doc.GetUnitSystemName(True, True, True, False)
+    ## fromating output decimal places not working
+    # decimalPlaces = doc.DistanceDisplayPrecision
+    # textOut = "Parallel Distance = {:." + str(decimalPlaces) +"f} " + unitsName
+    rs.ClipboardText(ParallelDistance)
+    textOut = "Parallel Distance = {:.4f} " + unitsName
+    # msgOut(textOut.format(ParallelDistance))
+    rs.MessageBox("the value is saved in your clipboard", 64, textOut.format(ParallelDistance))
+    print(textOut.format(ParallelDistance))
+    return rc.Commands.Result.Success
 
 def msgOut(_msg):
     print(_msg)
@@ -156,19 +181,19 @@ def ArePlanesParallel(_plane1, _plane2, tol):
     return False
 
 def clean_up_fail():
-    if UnlockLayersUsed:
+    if isUnlocked:
         lock_objects(objectsToRelock)
         lock_layers(layersToRelock)
     return
 
 def clean_up_success():
-    if UnlockLayersUsed:
+    if isUnlocked:
         lock_objects(objectsToRelock)
         lock_layers(layersToRelock)
     return
 
 def clean_up_cancel():
-    if UnlockLayersUsed:
+    if isUnlocked:
         lock_objects(objectsToRelock)
         lock_layers(layersToRelock)
     return
